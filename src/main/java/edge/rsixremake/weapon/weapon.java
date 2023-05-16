c22e415e706743909d3a91c66bf67c9b5c52b464
akaChenshunCodeMaster <91108691+akaChenshunCodeMaster@users.noreply.github.com>
2023-05-16 14:40:15 -0700

    update

--- src/main/java/edge/rsixremake/weapon/weapon.java
+++ src/main/java/edge/rsixremake/weapon/weapon.java
@@ -1,8 +1,11 @@
 package edge.rsixremake.weapon;
 
 import java.util.function.Supplier;
-
+import edge.rsixremake.rsixmain;
 
 public class weapon {
+
+	public static final String ITEMS = null;
+
 
 }
