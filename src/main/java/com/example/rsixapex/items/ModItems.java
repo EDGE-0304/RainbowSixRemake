5e8eb6f0447e1cc273b94c3d73b02fa3b3362b18
dlharry <dlharry@126.com>
2023-06-17 00:40:00 +0100

    add test_gun to the game under 'combat' tab

---
+++ src/main/java/com/example/rsixapex/items/ModItems.java
@@ -0,0 +1,22 @@
+package com.example.rsixapex.items;
+
+import com.example.rsixapex.RapexMod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+// The value here should match an entry in the META-INF/mods.toml file
+public class ModItems
+{
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RapexMod.MODID);
+	
+	public static final RegistryObject<Item> TEST_GUN = ITEMS.register(
+			"test_gun", () -> new Item(new Item.Properties()));
+	
+    public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
+}
