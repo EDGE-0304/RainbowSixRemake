4e5e27d46993e86b75cd0c74c269aa32bbe8e6c6
dlharry <dlharry@126.com>
2023-06-21 22:59:56 +0100

    replace gun model

--- src/main/java/com/example/rsixapex/items/Bullet.java
+++ src/main/java/com/example/rsixapex/items/Bullet.java
@@ -3,7 +3,6 @@ package com.example.rsixapex.items;
 import com.example.rsixapex.entity.ModEntityType;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.damagesource.DamageSource;
@@ -13,21 +12,12 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.monster.Blaze;
-import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.projectile.Fireball;
-import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.entity.projectile.Snowball;
-import net.minecraft.world.item.SnowballItem;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.registries.RegistryObject;
-import net.minecraft.world.item.FlintAndSteelItem;
 
 public class Bullet extends ThrowableItemProjectile {  
 	
