6a1e058839f22185c3b3d59784fc404746e1c6c0
akaChenshunCodeMaster <91108691+akaChenshunCodeMaster@users.noreply.github.com>
2023-06-24 11:32:40 -0700

    Update detect_arrow.java

--- src/main/java/com/example/rsixapex/items/detect_arrow.java
+++ src/main/java/com/example/rsixapex/items/detect_arrow.java
@@ -1,5 +1,32 @@
 package com.example.rsixapex.items;
 
+import com.example.rsixapex.entity.ModEntityType;
+
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Fireball;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.Snowball;
+import net.minecraft.world.item.SnowballItem;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.registries.RegistryObject;
+
+
 public class detect_arrow {
+	
+	
 
 }
